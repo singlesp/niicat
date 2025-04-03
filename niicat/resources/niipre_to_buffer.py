40ab55c4ece6fda98f9a045ea02e68a0d200a1d9
Parker Singleton <sps253@cornell.edu>
2025-04-03 18:11:49 -0400

    debug

--- niicat/resources/niipre_to_buffer.py
+++ niicat/resources/niipre_to_buffer.py
@@ -2,9 +2,15 @@ import sys
 import matplotlib.pyplot as plt
 from niicat.plotter import plot
 
+print(f"DEBUG: Received sys.argv: {sys.argv}", file=sys.stderr)
+
 iFile = sys.argv[1]
 dpi = int(sys.argv[2])
 slice_num = int(sys.argv[3]) if len(sys.argv) > 3 and sys.argv[3] else None
 volume_num = int(sys.argv[4]) if len(sys.argv) > 4 and sys.argv[4] else None
+
+print(f"DEBUG: Parsed slice_num: {slice_num}", file=sys.stderr)
+print(f"DEBUG: Parsed volume_num: {volume_num}", file=sys.stderr)
+
 fig = plot(iFile, return_fig=True, dpi=dpi, slice_num=slice_num, volume_num=volume_num)
 plt.savefig(sys.stdout.buffer, dpi=dpi)
