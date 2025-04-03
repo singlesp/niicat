c92f3d6225f902b52da7168cd5949828143cb411
Parker Singleton <sps253@cornell.edu>
2025-04-03 16:48:54 -0400

    arg

--- niicat/resources/niipre_to_buffer.py
+++ niicat/resources/niipre_to_buffer.py
@@ -5,5 +5,6 @@ from niicat.plotter import plot
 iFile = sys.argv[1]
 dpi = int(sys.argv[2])
 slice_num = int(sys.argv[3]) if len(sys.argv) > 3 and sys.argv[3] else None
-fig = plot(iFile, return_fig=True, dpi=dpi, slice_num=slice_num)
+volume_num = int(sys.argv[4]) if len(sys.argv) > 4 and sys.argv[4] else None
+fig = plot(iFile, return_fig=True, dpi=dpi, slice_num=slice_num, volume_num=volume_num)
 plt.savefig(sys.stdout.buffer, dpi=dpi)
